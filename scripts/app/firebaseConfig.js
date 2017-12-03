211b329aa5d17c539ef72825956640e18582cbe6
Paul-Claudiu Orha <orha.paul@gmail.com>
2017-12-03 15:38:30 +0200

    Update firebaseConfig.js

--- scripts/app/firebaseConfig.js
+++ scripts/app/firebaseConfig.js
@@ -15,5 +15,19 @@ $(document).ready(function(){
 
     };
 
-    firebase.initializeApp(config);
-})
+    firebase.initializeApp(config);})
+})})
+
+<script src="https://www.gstatic.com/firebasejs/4.7.0/firebase.js"></script>
+<script>
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyBXKUj-l68XEOp8nGgDegAw6i2eID1ElRU",
+    authDomain: "carbulator-8bd91.firebaseapp.com",
+    databaseURL: "https://carbulator-8bd91.firebaseio.com",
+    projectId: "carbulator-8bd91",
+    storageBucket: "carbulator-8bd91.appspot.com",
+    messagingSenderId: "860594250843"
+  };
+  firebase.initializeApp(config);
+</script>
