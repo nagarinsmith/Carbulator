cb0f80971d37d9f8f8c830ab76eb701aea2ed917
dex4 <cata_dc98@yahoo.com>
2017-12-10 02:53:08 +0200

    Chart style DONE

---
+++ drjn_style42/scripts/app/firebaseConfig.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+   // Initialize Firebase
+    var config = {
+        apiKey: "AIzaSyBXKUj-l68XEOp8nGgDegAw6i2eID1ElRU",
+        authDomain: "carbulator-8bd91.firebaseapp.com",
+        databaseURL: "https://carbulator-8bd91.firebaseio.com",
+        projectId: "carbulator-8bd91",
+        storageBucket: "carbulator-8bd91.appspot.com",
+        messagingSenderId: "860594250843"
+    };
+
+    firebase.initializeApp(config);
+
+    var rootRef = firebase.database().ref();
+})
